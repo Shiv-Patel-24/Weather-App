226f566e9f476511ac0bcd777f13489d7a79cec9
Shiv-Patel-24 <pshiv2411@gmail.com>
2025-06-17 09:54:12 +0530

    .env fiel problem solve

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,8 +7,8 @@ export default function SearchBox({updateInfo}) {
   let [city, setCity] = useState("");
   let [error, setError] = useState(false);
 
-    const API_URL = "https://api.openweathermap.org/data/2.5/weather"
-    const API_KEY = "32d526ca3351497682c9942ea4a03ea7"
+    const API_URL = import.meta.env.VITE_API_URL
+    const API_KEY = import.meta.env.VITE_API_KEY
 
     let getWeatherInfo = async() =>{
       try{
